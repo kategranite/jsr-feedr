6ba69bcef7e9a385d7a34578be9d75f92f3d8990
kategranite <41632836+kategranite@users.noreply.github.com>
2019-04-18 21:32:47 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
-/*
-  Please add all Javascript code to this file.
+const APP_KEY = 'af91509e664f477fba0ed9507826e6b8';
+const xhr = new XMLHttpRequest();
+
+function getArticles(ingredient) {
+xhr.open('GET', `https://newsapi.org/v2/everything?q=urban sustainability&from=2019-04-01&sortBy=publishedAt&apiKey=af91509e664f477fba0ed9507826e6b8`);
+xhr.send();
+xhr.onload = onSuccess; 
   
-*/
+  
